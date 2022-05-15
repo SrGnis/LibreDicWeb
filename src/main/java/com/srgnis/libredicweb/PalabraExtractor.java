d1a22b85e2aa1be0d37947ff728a17b517e10e0f
srgnis <gnisms97@gmail.com>
2022-05-15 21:18:55 +0200

    Al buscar palabras se devuleven ordenadas alfabeticamente

--- src/main/java/com/srgnis/libredicweb/PalabraExtractor.java
+++ src/main/java/com/srgnis/libredicweb/PalabraExtractor.java
@@ -6,11 +6,12 @@ import com.srgnis.libredicweb.models.Sentido;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 
-import java.lang.reflect.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.text.Collator;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.TreeMap;
 
 public class PalabraExtractor implements ResultSetExtractor<Palabra[]> {
     
@@ -19,15 +20,15 @@ public class PalabraExtractor implements ResultSetExtractor<Palabra[]> {
 
         Palabra p = null;
         Sentido s;
-        HashMap<String, Palabra> palabrasHashMap = new HashMap<>();
+        TreeMap<String, Palabra> palabrasTree = new TreeMap<>(Collator.getInstance(new Locale("es", "ES")));
         HashMap<Long, Sentido> sentidoHashMap = new HashMap<>();
 
         int cont = 0;
         while (rs.next()){
-        	p = palabrasHashMap.get(rs.getString("lema"));
+        	p = palabrasTree.get(rs.getString("lema"));
             if(p == null){
                 p = new Palabra(rs.getString("lema"));
-                palabrasHashMap.put(rs.getString("lema"), p);
+                palabrasTree.put(rs.getString("lema"), p);
             }
 
             s = sentidoHashMap.get(rs.getLong("id_sentido"));
@@ -40,6 +41,6 @@ public class PalabraExtractor implements ResultSetExtractor<Palabra[]> {
 
             s.caracteristicas.propiedades.add(new Propiedad(rs.getString("propiedad"), rs.getString("valor"), s.caracteristicas));
         }
-        return palabrasHashMap.values().toArray(new Palabra[palabrasHashMap.values().size()]);
+        return palabrasTree.values().toArray(new Palabra[palabrasTree.values().size()]);
     }
 }
